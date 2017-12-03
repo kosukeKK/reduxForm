a0aa232d8409c3a8ace7abd4c46073bee1cb8791
kosukeKK <kosuke.kato@jmty.jp>
2017-12-03 17:41:15 +0900

    data

--- app/javascript/packs/components/App.tsx
+++ app/javascript/packs/components/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import ContactForm from '../components/Form';
 //import { addUser } from '../actions'
-
+import axios from 'axios';
 const App = (that: any) => (
     <div>
         hello
@@ -11,6 +11,13 @@ const App = (that: any) => (
 );
 
 const submitData = (dispatch: any, res: any) => {
+    axios.post('url', res)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
     dispatch({ type: 'ADD_USER', res });
 };
 
@@ -18,3 +25,10 @@ const mapStateToProps = (state: number) => {
     return { state };
 };
 export default connect(mapStateToProps)(App);
+
+
+// axios.get('https://jsonplaceholder.typicode.com/comments').then((r) => (
+//     console.log(r)
+// )).catch((e) => (
+//     console.error(e)
+// ));
